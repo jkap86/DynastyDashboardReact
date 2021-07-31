e72f41d2ab8b3aecf1ae5448e0e18541a9530b5a
jkap86 <jkap86@gmail.com>
2021-07-31 11:39:02 -0400

    fix for deployment to heroku

--- api/app.py
+++ api/app.py
@@ -5,7 +5,11 @@ import concurrent.futures
 import re
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../build', static_url_path='/')
+
+@app.route('/')
+def index():
+	return app.send_static_file('index.html')
 
 @app.route('/dynastyvalues')
 def get_dynasty_values():
