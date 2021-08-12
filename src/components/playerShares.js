2914e000820425f413563b286f0719b2221aa260
jkap86 <jkap86@gmail.com>
2021-08-12 16:52:03 -0400

    add filter by draft year to playershares

--- src/components/playerShares.js
+++ src/components/playerShares.js
@@ -16,13 +16,24 @@ class PlayerShares extends Component {
 			players: [],
 			playersDict: [],
 			avatar: '',
-			qbcheck: true,
-			rbcheck: true,
-			wrcheck: true,
-			techeck: true
+			QB: true,
+			RB: true,
+			WR: true,
+			TE: true,
+			rookie: true,
+			one: true,
+			two: true,
+			three: true,
+			four: true,
+			five: true,
+			six: true,
+			seven: true,
+			eight: true,
+			nine: true,
 
 		}
 		this.filterPosition = this.filterPosition.bind(this)
+		this.filterYear = this.filterYear.bind(this)
 	}
 
 	filterPosition(e) {
@@ -30,7 +41,19 @@ class PlayerShares extends Component {
 		const name = e.target.name
 		let positionSelected = document.getElementsByClassName(position)
 		for (let i = 0; i < positionSelected.length; i++) {
-			positionSelected[i].style.display = positionSelected[i].style.display === 'none' ? 'table-row' : 'none' 
+			positionSelected[i].style.display = e.target.checked === true && this.state[positionSelected[i].getAttribute('class').split(' ')[3]] === true ? 'table-row' : 'none' 
+		}
+		this.setState({
+			[name]: e.target.checked
+		})
+	}
+
+	filterYear(e) {
+		const year = e.target.value
+		const name = e.target.name
+		let yearsSelected = document.getElementsByClassName(name)
+		for (let i = 0; i < yearsSelected.length; i++) {
+			yearsSelected[i].style.display = e.target.checked === true && this.state[yearsSelected[i].getAttribute('class').split(' ')[2]] ? 'table-row' : 'none'
 		}
 		this.setState({
 			[name]: e.target.checked
@@ -92,13 +115,27 @@ class PlayerShares extends Component {
 			<Theme/>
 			<h1><img src={this.state.avatar}/>{this.state.username} Player Shares</h1>
 			<h2>{this.state.leagues.length} Leagues</h2>
+			<h3>
 			<label>
-				Position
-				QB <input value="QB" name="qbcheck" checked={this.state.qbcheck} onChange={this.filterPosition} type="checkbox"/>
-				RB <input value="RB" name="rbcheck" checked={this.state.rbcheck} onChange={this.filterPosition} type="checkbox"/>
-				WR <input value="WR" name="wrcheck" checked={this.state.wrcheck} onChange={this.filterPosition} type="checkbox"/>
-				TE <input value="TE" name="techeck" checked={this.state.techeck} onChange={this.filterPosition} type="checkbox"/>
+				<input value="QB" name="QB" checked={this.state.QB} onChange={this.filterPosition} type="checkbox"/>QB
+				<input value="RB" name="RB" checked={this.state.RB} onChange={this.filterPosition} type="checkbox"/>RB
+				<input value="WR" name="WR" checked={this.state.WR} onChange={this.filterPosition} type="checkbox"/>WR
+				<input value="TE" name="TE" checked={this.state.TE} onChange={this.filterPosition} type="checkbox"/>TE
 			</label>
+			<br/>
+			<label>
+				<input onChange={this.filterYear} checked={this.state.rookie} value="rookie" name="rookie" type="checkbox"/> 2021
+				<input onChange={this.filterYear} checked={this.state.one} value="2020" name="one" type="checkbox"/> 2020
+				<input onChange={this.filterYear} checked={this.state.two}  value="2019" name="two" type="checkbox"/> 2019
+				<input onChange={this.filterYear} checked={this.state.three} value="2018" name="three" type="checkbox"/> 2018
+				<input onChange={this.filterYear} checked={this.state.four} value="2017" name="four" type="checkbox"/> 2017
+				<input onChange={this.filterYear} checked={this.state.five} value="2016" name="five" type="checkbox"/> 2016
+				<input onChange={this.filterYear} checked={this.state.six} value="2015" name="six" type="checkbox"/> 2015
+				<input onChange={this.filterYear} checked={this.state.seven} value="2014" name="seven" type="checkbox"/> 2014
+				<input onChange={this.filterYear} checked={this.state.eight} value="2013" name="eight" type="checkbox"/> 2013
+				<input onChange={this.filterYear} checked={this.state.nine} value="2012" name="nine" type="checkbox"/> 2012
+			</label>
+			</h3>
 			<table>
 				<thead>
 					<tr>
@@ -111,7 +148,7 @@ class PlayerShares extends Component {
 				</thead>
 				<tbody>
 				{this.state.playersDict.sort((a, b) => (a.count < b.count) ? 1 : -1).map(player => 
-					<tr key={player.name} className={`row player ${allPlayers[player.name].position}`}>
+					<tr key={player.name} className={`row player ${allPlayers[player.name].position} ${allPlayers[player.name].years_exp === 0 ? 'rookie' : allPlayers[player.name].years_exp === 1 ? 'one' : allPlayers[player.name].years_exp === 2 ? 'two' : allPlayers[player.name].years_exp === 3 ? 'three' : allPlayers[player.name].years_exp === 4 ? 'four' : allPlayers[player.name].years_exp === 5 ? 'five' : allPlayers[player.name].years_exp === 6 ? 'six' : allPlayers[player.name].years_exp === 7 ? 'seven' : allPlayers[player.name].years_exp === 8 ? 'eight' : 'nine'}`}>
 						<td>{allPlayers[player.name].position + " " + allPlayers[player.name].first_name + " " + allPlayers[player.name].last_name + " " + allPlayers[player.name].team}</td>
 						<td>{allPlayers[player.name].age}</td>
 						<td>{allPlayers[player.name].college}</td>
