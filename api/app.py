50a43e025705b3155b32f677e1069eac31b7dba3
jkap86 <jkap86@gmail.com>
2021-09-28 16:04:25 -0400

    add catch all route to api/app.py

--- api/app.py
+++ api/app.py
@@ -10,7 +10,7 @@ app = Flask(__name__, static_folder='../build', static_url_path='/')
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-	return app.send_static_file('index.html')
+	print(repr(path))
 
 
 @app.route('/')
