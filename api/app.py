0a105c3904c66ff34b3ef2cdebfdddb07e626c1e
jkap86 <jkap86@gmail.com>
2021-10-01 12:33:31 -0400

    app routes

--- api/app.py
+++ api/app.py
@@ -9,7 +9,7 @@ app = Flask(__name__, static_folder='../build', static_url_path='/')
 
 @app.route('/leagues/<path:path>', defaults={'path': ''})
 @app.route('/')
-def index():
+def index(path):
 	return app.send_static_file('index.html')
 
 
