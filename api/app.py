3f609899e1363dab7752bd5dfbd3e5aae475c5e0
jkap86 <jkap86@gmail.com>
2021-10-01 16:26:08 -0400

    app routes

--- api/app.py
+++ api/app.py
@@ -7,12 +7,6 @@ import re
 
 app = Flask(__name__, static_folder='../build', static_url_path='/')
 
-@app.route('/', defaults={'path1': '', 'path2': ''})
-@app.route('/<path:path1>', defaults={'path2', ''})
-@app.route('/<path:path1>/<path:path2>')
-def catch_all(path1, path2):
-	return redirect(url_for('index'))
-
 @app.route('/')
 def index():
 	return app.send_static_file('index.html')
