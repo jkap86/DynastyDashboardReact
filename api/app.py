1e5da1a8e918e0c30ca79e862bc4db707450b04a
jkap86 <jkap86@gmail.com>
2021-10-01 13:16:35 -0400

    app routes

--- api/app.py
+++ api/app.py
@@ -5,17 +5,11 @@ import concurrent.futures
 import re
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../build', static_url_path='/')
 
 
 @app.route('/')
 def index():
-	return app.send_static_file('index.html')
-
-@app.route('/', defaults={'path1': '', 'path2': ''})
-@app.route('/<path:path1>', defaults={'path2': ''})
-@app.route('/<path:path1>/<path:path2>')
-def catch_all(path1, path2):
 	return app.send_static_file('index.html')
 
 @app.route('/dynastyvalues')
