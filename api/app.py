5463ab4139d344d87bf19ab504bc4297d496ae44
jkap86 <jkap86@gmail.com>
2021-10-05 17:38:28 -0400

    app.py

--- api/app.py
+++ api/app.py
@@ -12,6 +12,7 @@ app = Flask(__name__, static_folder='../build', static_url_path='/')
 def index():
 	return app.send_static_file('index.html')
 
+@app.route('/commonleagues/<path>')
 @app.route('/playershares/<path>')
 @app.route('/leagues/<path>')
 def catch_all(path):
