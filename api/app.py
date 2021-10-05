ab418c776298d5ba2e482003647ad282426c2758
jkap86 <jkap86@gmail.com>
2021-10-05 16:58:39 -0400

    app.py

--- api/app.py
+++ api/app.py
@@ -8,9 +8,8 @@ import re
 app = Flask(__name__, static_folder='../build', static_url_path='/')
 
 @app.route('/', defaults={'path': ''})
-@app.route('/<path:path>' defaults={'path2': ''})
-@app.route('/<path:path>/<path:path2>')
-def catch_all(path, path2):
+@app.route('/<path:path>/<path:path>')
+def catch_all(path):
 	return app.send_static_file('index.html')
 
 @app.route('/dynastyvalues')
